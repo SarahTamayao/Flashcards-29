8276a581346944df79e01099f1c55f4b411f5e45
innocentmunai <innocentmunai@gmail.com>
2022-03-19 02:36:18 -0400

    Completed lab 2 optionals

--- flashcards/CreationViewController.swift
+++ flashcards/CreationViewController.swift
@@ -10,11 +10,17 @@ import UIKit
 class CreationViewController: UIViewController {
     
     var flashcardsController: ViewController!
+    
+    var initialQuestion: String!
+    var initialAnswer: String!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        questionTextField.text = initialQuestion
+        answerTextField.text = initialAnswer
+
     }
     
 
@@ -35,20 +41,37 @@ class CreationViewController: UIViewController {
     // IBOutlest for the question and answer text fields objects
     @IBOutlet weak var questionTextField: UITextField!
     @IBOutlet weak var answerTextField: UITextField!
+    @IBOutlet weak var extraOneTextField: UITextField!
+    @IBOutlet weak var extraTwoTextField: UITextField!
     
     @IBAction func didTapOnDone(_ sender: Any) {
         
-        // Get the question
+        // Get the question and answers
         let questionText = questionTextField.text
-        
-        // Get the answer
         let answerText = answerTextField.text
+        let extraOneText = extraOneTextField.text
+        let extraTwoText = extraTwoTextField.text
         
+        // Create alerts for missing question and answer
+        let alert = UIAlertController(title: "Missing text", message: "You need both a question and an answer" , preferredStyle: UIAlertController.Style .alert)
+        
+        // Ok action created and added to the alerts
+        let okAction = UIAlertAction(title: "Ok", style: .default)
+        alert.addAction(okAction)
+        
+        
+        if questionText == nil || questionText!.isEmpty || answerText == nil || answerText!.isEmpty {
+            present(alert, animated: true)
+        }
+        else{
+            
         // Call the function to update the flashcard
-        flashcardsController.updateFlashcard(question: questionText!, answer: answerText!)
+        flashcardsController.updateFlashcard(question: questionText!, answer: answerText!, extraAnswerOne: extraOneText, extraAnswerTwo: extraTwoText)
+        }
         
         // Dismiss
         dismiss(animated: true)
+        
     }
     
 }
--- flashcards/ViewController.swift
+++ flashcards/ViewController.swift
@@ -11,13 +11,46 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var backLabel: UILabel!
     @IBOutlet weak var frontLabel: UILabel!
+    @IBOutlet weak var card: UIView!
+    
+    
+    @IBOutlet weak var btnOptionOne: UIButton!
+    @IBOutlet weak var btnOptionTwo: UIButton!
+    @IBOutlet weak var btnOptionThree: UIButton!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // Give card it round corners
+        card.layer.cornerRadius = 20.0
+        card.layer.shadowRadius = 15.0
+        card.layer.shadowOpacity = 0.2
+        
+        // Give labels rounded corners
+        backLabel.layer.cornerRadius = 20.0
+        frontLabel.layer.cornerRadius = 20.0
+        frontLabel.clipsToBounds = true
+        backLabel.clipsToBounds = true
+        
+
+        
         // Do any additional setup after loading the view.
     }
 
+    
+    // Support multiple choice answer
+    @IBAction func didTapOptionOne(_ sender: Any) {
+        btnOptionOne.isHidden = true
+    }
+    @IBAction func didTapOptionTwo(_ sender: Any) {
+        frontLabel.isHidden = true
+        
+    }
+    @IBAction func didTapOptionThree(_ sender: Any) {
+        btnOptionThree.isHidden = true
+    }
+    
     
     @IBAction func didTapOnFlashcard(_ sender: Any) {
         if frontLabel.isHidden {
@@ -26,14 +59,24 @@ class ViewController: UIViewController {
         else {
             frontLabel.isHidden = true
         }
+    }
+    
+    
+    func updateFlashcard(question: String, answer: String, extraAnswerOne: String?, extraAnswerTwo: String?) {
+        frontLabel.text = question
+        backLabel.text = answer
+        
+        btnOptionOne.setTitle(extraAnswerOne, for: .normal)
+        btnOptionTwo.setTitle(answer, for: .normal)
+        btnOptionThree.setTitle(extraAnswerTwo, for: .normal)
+        
+        // Set the flashcard to default visibility
+        frontLabel.isHidden = false
+        btnOptionOne.isHidden = false
+        btnOptionThree.isHidden = false
         
     }
     
-    
-    func updateFlashcard(question: String, answer: String) {
-        frontLabel.text = question
-        backLabel.text = answer
-    }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         
@@ -45,8 +88,13 @@ class ViewController: UIViewController {
         
         // We set the flashcardsController property to self
         creationController.flashcardsController=self
+        
+        
+        if segue.identifier == "EditSegue" {
+            creationController.initialQuestion = frontLabel.text
+            creationController.initialAnswer = backLabel.text
+        }
     }
-    
     
 }
 
